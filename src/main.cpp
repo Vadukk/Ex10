01e2d5d6f6553f4768bf95e1395c38269d6e548b
Vadukk <Vadik-Tumakov.com@yandex.ru>
2021-01-19 10:44:21 +0300

    try 2

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,6 @@ int main() {
   std::string infix = "3 + 8 * 1 / (2 - 1) + 9 * 8 * 9 / 3";
   std::cout << infix << std::endl;
   std::string postfix = infix2postfix(infix);
-  std::cout << postfix << std::endl; 
+  std::cout << postfix << std::endl;
   return 0;
 }
